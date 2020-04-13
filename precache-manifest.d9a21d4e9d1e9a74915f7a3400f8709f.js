9ed2b86d42da3d3123d627b2f28a858ce90ecc41
cielolili <cielito.torres@hotmail.com>
2020-04-12 22:23:35 -0500

    Updates

---
+++ precache-manifest.d9a21d4e9d1e9a74915f7a3400f8709f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f56d822847948a285f83793ae176c4",
+    "url": "/LIM011-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "46e47eeee86736dbd8b2",
+    "url": "/LIM011-fe-burger-queen/static/css/main.da2dd87d.chunk.css"
+  },
+  {
+    "revision": "5021961aeb1e3f3e4c1f",
+    "url": "/LIM011-fe-burger-queen/static/js/2.6d4e4358.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/LIM011-fe-burger-queen/static/js/2.6d4e4358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e47eeee86736dbd8b2",
+    "url": "/LIM011-fe-burger-queen/static/js/main.1ef3b9b0.chunk.js"
+  },
+  {
+    "revision": "da9d47fbcf59f4493dda",
+    "url": "/LIM011-fe-burger-queen/static/js/runtime-main.5c06db22.js"
+  },
+  {
+    "revision": "74fb79b510838e92da485cc4b867b795",
+    "url": "/LIM011-fe-burger-queen/static/media/cheeseburger.74fb79b5.svg"
+  }
+]);
